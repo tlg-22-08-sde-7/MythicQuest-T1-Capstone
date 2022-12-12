4ff1bc583f05d7fed84a847bbeec1eb54983620a
Coel316 <coel316@yahoo.com>
2022-12-12 10:04:12 -0800

    Added JSON functionality AB#350, AB#355, AB#361, AB#363

---
+++ src/com/mythicquest/engine/Dialogue.java
@@ -0,0 +1,54 @@
+package com.mythicquest.engine;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Dialogue {
+
+    // TESTING ExitGame from MythicQuestApp.java
+
+    // Used Buffered Reader to re-use it, from line 23 (in MysticQuestApp)
+    public static void exitGame(int exitStatus, BufferedReader scan) throws IOException {
+        if (scan == null) {
+            scan = new BufferedReader(new InputStreamReader(System.in));
+        }
+        Reader reader = Files.newBufferedReader(Paths.get("resources/JSON/exitGame.json"));
+        JsonObject parser = JsonParser.parseReader(reader).getAsJsonObject();
+
+        // System.out.println("Oh hey!  Are you wanting to exit the game?");
+        System.out.println(parser.get("confirmExit").getAsString());
+
+        String exitChoice = scan.readLine();
+
+        if (exitChoice.toLowerCase().contains("yes")) {
+            // Terminate JVM
+            // By convention, nonzero status code indicates abnormal termination
+            System.out.println("You got it - ending the game now...  See you again soon? :)");
+            System.exit(exitStatus);
+        }
+    }
+}
+
+
+//        public static void exitGame () {
+//
+//            // create a reader
+//            try {
+//                Reader reader = Files.newBufferedReader(Paths.get("resources/JSON/exitGame.json"));
+//                JsonObject parser = JsonParser.parseReader(reader).getAsJsonObject();
+//                String confirmExit = parser.get("confirmExit").getAsString();
+//                String exitConfirmed = parser.get("exitConfirmed").getAsString();
+//                System.out.println(confirmExit);
+//                System.out.println(exitConfirmed);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+
---
+++ src/com/mythicquest/engine/Direction.java
@@ -0,0 +1,6 @@
+package com.mythicquest.engine;
+
+enum Direction {
+    // wanted directions to be quickly found, so I put in this ENUM.
+    NORTH, EAST, SOUTH, WEST
+}
