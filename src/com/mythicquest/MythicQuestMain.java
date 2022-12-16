4aa97368d515fb24fe166bb7113143a87eb02dab
Dorian Majano <dorian.majano@gmail.com>
2022-12-16 09:04:38 -0800

    Add TODO to MythicQuestMain.java AB#997

--- src/com/mythicquest/MythicQuestMain.java
+++ src/com/mythicquest/MythicQuestMain.java
@@ -4,6 +4,7 @@ import com.mythicquest.app.MythicQuestApp;
 
 public class MythicQuestMain {
 
+    // TODO: Handle exception before it gets to the main class
     public static void main(String[] args) throws Exception {
         MythicQuestApp app = new MythicQuestApp();
         app.start();
--- src/com/mythicquest/engine/Player.java
+++ src/com/mythicquest/engine/Player.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 class Player {
     private Location location;
-    private ArrayList<String > inventory;
+    private ArrayList<String> inventory;
     private int healthLevel;
 
     // ctor
@@ -20,29 +20,29 @@ class Player {
     }
 
     // Remove items from inventory
-        public void removeItem(String itemRemove) {
+    public void removeItem(String itemRemove) {
         inventory.remove(itemRemove);
-        }
+    }
 
     // Check items in inventory
-        public void checkItem() {
-            System.out.println("Current inventory: ");
-            for (int i = 0; i < inventory.size(); i++) {
-                System.out.println(inventory.get(i));
-            }
+    public void checkItem() {
+        System.out.println("Current inventory: ");
+        for (int i = 0; i < inventory.size(); i++) {
+            System.out.println(inventory.get(i));
         }
+    }
 
-        public void statusInfo() {
-            System.out.println();
-            System.out.println("Current health: " + healthLevel);
-            System.out.println("Location: " + location.moreInfo());
-            System.out.println("Description: " + location.getDescription());
-            System.out.println("Items available: " + location.getItems());
-            checkItem();
+    public void statusInfo() {
+        System.out.println();
+        System.out.println("Current health: " + healthLevel);
+        System.out.println("Location: " + location.moreInfo());
+        System.out.println("Description: " + location.getDescription());
+        System.out.println("Items available: " + location.getItems());
+        checkItem();
 
-            System.out.println();
+        System.out.println();
 
-        }
+    }
 
     public Location getLocation() {
         return location;
