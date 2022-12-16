d8f19aa24d4fb25b74ae6d3aac3a5e60569bae39
jennytoc <toctocanj@gmail.com>
2022-12-16 13:48:59 -0800

    Adding more to GamePanel AB#596

--- src/com/mythicquest/GamePanel.java
+++ src/com/mythicquest/GamePanel.java
@@ -7,6 +7,7 @@ public class GamePanel {
     JFrame window;
     Container con;
     JPanel titleNamePanel;
+    JLabel titleNameLabel;
 
     public static void main(String[] args) {
         new GamePanel();
@@ -19,11 +20,15 @@ public class GamePanel {
         window.getContentPane().setBackground(Color.black);
         window.setLayout(null);
         window.setVisible(true);
+        window.setLocationRelativeTo(null);
         con = window.getContentPane();
 
         titleNamePanel = new JPanel();
-        titleNamePanel.setBounds(100, 100, 600, 150); // Space around text
+        titleNamePanel.setBounds(100, 100, 600, 150); // Space around text, width and height
         titleNamePanel.setBackground(Color.blue);
-
+        titleNameLabel = new JLabel("MYTHIC QUEST");
+        titleNameLabel.setForeground(Color.white);
+        titleNamePanel.add(titleNameLabel);
+        con.add(titleNamePanel);
     }
 }
