a8400fa983181d9dbdb964bd85f188d14ef6186c
jennytoc <toctocanj@gmail.com>
2022-12-16 14:28:44 -0800

    Window shows title AB#596

--- src/com/mythicquest/GamePanel.java
+++ src/com/mythicquest/GamePanel.java
@@ -6,8 +6,9 @@ import java.awt.*;
 public class GamePanel {
     JFrame window;
     Container con;
-    JPanel titleNamePanel;
-    JLabel titleNameLabel;
+    JPanel topNamePanel, bottomNamePanel;
+    JLabel topNameLabel, bottomNameLabel;
+    Font titleFont = new Font("Times New Roman", Font.PLAIN, 90);
 
     public static void main(String[] args) {
         new GamePanel();
@@ -23,12 +24,24 @@ public class GamePanel {
         window.setLocationRelativeTo(null);
         con = window.getContentPane();
 
-        titleNamePanel = new JPanel();
-        titleNamePanel.setBounds(100, 100, 600, 150); // Space around text, width and height
-        titleNamePanel.setBackground(Color.blue);
-        titleNameLabel = new JLabel("MYTHIC QUEST");
-        titleNameLabel.setForeground(Color.white);
-        titleNamePanel.add(titleNameLabel);
-        con.add(titleNamePanel);
+        topNamePanel = new JPanel();
+        topNamePanel.setBounds(100, 100, 600, 110); // Space around text, width and height
+        topNamePanel.setBackground(Color.black);
+        bottomNamePanel = new JPanel();
+        bottomNamePanel.setBounds(100, 210, 600, 100);
+        bottomNamePanel.setBackground(Color.black);
+
+        topNameLabel = new JLabel("MYTHIC");
+        topNameLabel.setForeground(Color.white);
+        topNameLabel.setFont(titleFont);
+        bottomNameLabel = new JLabel("QUEST");
+        bottomNameLabel.setForeground(Color.white);
+        bottomNameLabel.setFont(titleFont);
+
+        // Add label to panels, panels to containers in order to be visible
+        topNamePanel.add(topNameLabel);
+        bottomNamePanel.add(bottomNameLabel);
+        con.add(topNamePanel);
+        con.add(bottomNamePanel);
     }
 }
