26a4e964cf3c6bd54047129c0f5ebf23d889a687
jennytoc <toctocanj@gmail.com>
2022-12-16 15:17:38 -0800

    Added read and start button AB#596

--- src/com/mythicquest/GamePanel.java
+++ src/com/mythicquest/GamePanel.java
@@ -6,9 +6,11 @@ import java.awt.*;
 public class GamePanel {
     JFrame window;
     Container con;
-    JPanel topNamePanel, bottomNamePanel;
+    JPanel topNamePanel, bottomNamePanel, readPanel, startPanel;
     JLabel topNameLabel, bottomNameLabel;
+    JButton readBtn, startBtn;
     Font titleFont = new Font("Times New Roman", Font.PLAIN, 90);
+    Font btnFont = new Font("Times New Roman", Font.PLAIN, 30);
 
     public static void main(String[] args) {
         new GamePanel();
@@ -24,24 +26,49 @@ public class GamePanel {
         window.setLocationRelativeTo(null);
         con = window.getContentPane();
 
+        // Title panel and text (top)
         topNamePanel = new JPanel();
-        topNamePanel.setBounds(100, 100, 600, 110); // Space around text, width and height
+        topNamePanel.setBounds(100, 80, 600, 110); // Space around text, width and height
         topNamePanel.setBackground(Color.black);
-        bottomNamePanel = new JPanel();
-        bottomNamePanel.setBounds(100, 210, 600, 100);
-        bottomNamePanel.setBackground(Color.black);
-
         topNameLabel = new JLabel("MYTHIC");
-        topNameLabel.setForeground(Color.white);
+        topNameLabel.setForeground(Color.white); // font color
         topNameLabel.setFont(titleFont);
+
+        // Title panel and text (bottom)
+        bottomNamePanel = new JPanel();
+        bottomNamePanel.setBounds(100, 195, 600, 100);
+        bottomNamePanel.setBackground(Color.black);
         bottomNameLabel = new JLabel("QUEST");
         bottomNameLabel.setForeground(Color.white);
         bottomNameLabel.setFont(titleFont);
 
+        // Read panel and button
+        readPanel = new JPanel();
+        readPanel.setBounds(310, 330, 250, 80);
+        readPanel.setBackground(Color.black);
+        readBtn = new JButton("Read Instructions");
+        readBtn.setBackground(Color.black);
+        readBtn.setForeground(Color.white);
+        readBtn.setFont(btnFont);
+
+        // Start panel and button
+        startPanel = new JPanel();
+        startPanel.setBounds(310, 400, 250, 80);
+        startPanel.setBackground(Color.black);
+        startBtn = new JButton("Start Game");
+        startBtn.setBackground(Color.black);
+        startBtn.setForeground(Color.white);
+        startBtn.setFont(btnFont);
+
         // Add label to panels, panels to containers in order to be visible
         topNamePanel.add(topNameLabel);
         bottomNamePanel.add(bottomNameLabel);
+        readPanel.add(readBtn);
+        startPanel.add(startBtn);
+
         con.add(topNamePanel);
         con.add(bottomNamePanel);
+        con.add(readPanel);
+        con.add(startPanel);
     }
 }
