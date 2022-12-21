6a60a26af284797b1671049ba41b521c1b11cdd8
Dorian Majano <dorian.majano@gmail.com>
2022-12-21 14:18:52 -0800

    Refactor PlayerA class to use getters and setters AB#595

--- src/com/mythicquest/engine/Sprite.java
+++ src/com/mythicquest/engine/Sprite.java
@@ -1,8 +1,53 @@
 package com.mythicquest.engine;
 
 
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+
 class Sprite {
-    public int x;
-    public int y;
-    public int speed;
+    private int x;
+    private int y;
+    private int speed;
+    private BufferedImage playerImage;
+    private String direction;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setPlayerImage(BufferedImage playerImage) {
+        this.playerImage = playerImage;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public BufferedImage getPlayerImage() {
+        return playerImage;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
 }
