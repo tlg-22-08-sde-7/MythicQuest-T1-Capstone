f306666f2f6432e97f2a55ff68e1dbb22eb121b5
jennytoc <toctocanj@gmail.com>
2023-01-04 16:36:16 -0700

    Commented out first door AB#1062

--- src/com/mythicquest/app/AssetSetter.java
+++ src/com/mythicquest/app/AssetSetter.java
@@ -14,12 +14,12 @@ public class AssetSetter {
     public void setObject() {
         // Coordinates on world map
         gp.obj[0] = new Key();
-        gp.obj[0].worldX = 22 * gp.getScaledTileSize();
-        gp.obj[0].worldY = 20 * gp.getScaledTileSize();
+        gp.obj[0].worldX = 23 * gp.getScaledTileSize();
+        gp.obj[0].worldY = 7 * gp.getScaledTileSize();
 
-        gp.obj[1] = new Door();
-        gp.obj[1].worldX = 10 * gp.getScaledTileSize();
-        gp.obj[1].worldY = 11 * gp.getScaledTileSize();
+//        gp.obj[1] = new Door();
+//        gp.obj[1].worldX = 10 * gp.getScaledTileSize();
+//        gp.obj[1].worldY = 11 * gp.getScaledTileSize();
 
         gp.obj[2] = new Chest();
         gp.obj[2].worldX = 10 * gp.getScaledTileSize();
