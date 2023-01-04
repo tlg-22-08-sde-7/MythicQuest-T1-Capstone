6438526a172613497c640d1cfe6b057e5aefd2c8
jennytoc <toctocanj@gmail.com>
2023-01-04 11:49:28 -0700

    Changed world txt file AB#1062

--- src/com/mythicquest/object/SuperObject.java
+++ src/com/mythicquest/object/SuperObject.java
@@ -13,15 +13,12 @@ public class SuperObject {
     public Rectangle solidArea = new Rectangle(0, 0, 48, 48);
     public int solidAreaDefaultX = 0;
     public int solidAreaDefaultY = 0;
-    public int mapNumber;
 
     // Shows the objects on the map
     // public void draw(Graphics2D g2, GamePanel gp)
     public void draw(Graphics2D g2, GamePanel gp) {
         int screenX = worldX - gp.player.getWorldX() + gp.player.screenX;
         int screenY = worldY - gp.player.getWorldX() + gp.player.screenY;
-//        int screenX = gp.player.screenX;
-//        int screenY = gp.player.screenY;
 
         if (worldX + gp.getScaledTileSize() > gp.player.getWorldX() - gp.player.screenX &&
                 worldX - gp.getScaledTileSize() < gp.player.getWorldX() + gp.player.screenX &&
